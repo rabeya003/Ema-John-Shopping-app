59ce10a08e506d6541db6ce6d2cee173775fb0f3
rabeya003 <rabeya15-13483@diu.edu.bd>
2023-10-24 19:42:22 +0600

    retrieve product from stored cart and set it to cart state

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -17,8 +17,17 @@ const Shop = () => {
 
   useEffect(() => {
     const storedCart = getShoppingCart();
-    console.log(storedCart);
-  }, []);
+    const saveCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        saveCart.push(addedProduct);
+      }
+    }
+    setCart(saveCart);
+  }, [products]);
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
